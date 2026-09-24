0d6ad6ec8bd67b5bd115f4347b1a45f4480559bc
JosephMaltese <josephmaltese34@gmail.com>
2026-09-23 22:39:23 -0400

    Add email support

--- main.py
+++ main.py
@@ -5,10 +5,27 @@ from database import (
     job_exists,
     get_all_jobs
 )
+import json
+import smtplib
+from email.message import EmailMessage
+import os
+from dotenv import load_dotenv
 
+# Jobs will be represented as a dictionary containing the following keys:
+# id
+# company
+# posting_date
+# title
+# link
+# city
+# country
+# Note: Some keys may have to be optional depending on other apis
 
-def get_data():
-    # First, get Amazon jobs
+load_dotenv()
+
+def get_amazon_jobs():
+    amazon_jobs = []
+
     res = requests.post(
         "https://www.amazon.jobs/api/jobs/search?is_als=true",
         json={
@@ -47,12 +64,84 @@ def get_data():
     )
     print("Response status code:", res.status_code)
     json_data = res.json()
-    # print(json_data["searchHits"])
-    # insert_job(1, "Amazon", "Software Developer Intern")
-    all_jobs = get_all_jobs()
-    for job in all_jobs:
-        print(job)
+    job_list = json_data["searchHits"]
+
+    for job in job_list:
+        fields = job["fields"]
+        amazon_jobs.append({
+            "id": fields["icimsJobId"][0],
+            "company": "Amazon",
+            "posting_date": fields["createdDate"][0],
+            "title": fields["title"][0],
+            "link": fields["urlNextStep"][0],
+            "city": fields["city"][0],
+            "country": fields["country"][0]
+        })
+    return amazon_jobs
+
+
+def send_email(jobs):
+    message = EmailMessage()
+    message["From"] = os.getenv("EMAIL_ADDRESS")
+    message["To"] = os.getenv("RECIPIENT_EMAIL_ADDRESS")
+    message["Subject"] = f"💻 New SWE Internships! 🎉"
+    message_content = """
+        New internship postings found! Apply ASAP to give yourself the best chances.
+
+        """
+    for job in jobs:
+        message_content += f"""
+        {job["title"]}
+        {job["company"]}
+        {job["city"]}, {job["country"]}
+        Posting Date: {job["posting_date"]}
+        Apply Here: {job["link"]}
+
+        """
+    message.set_content(
+        message_content
+    )
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.login(
+            os.getenv("EMAIL_ADDRESS"),
+            os.getenv("EMAIL_APP_PASSWORD")
+        )
+
+        smtp.send_message(message)
 
 if __name__ == '__main__':
-    initialize_database()
-    get_data()
+    # new_jobs = []
+    # initialize_database()
+    # amazon_jobs = get_amazon_jobs()
+    
+    # for job in amazon_jobs:
+    #     if not job_exists(job["id"], job["company"]):
+    #         # Add job to alert list and then add to db
+    #         new_jobs.append(job)
+    #         insert_job(job["id"], job["company"], job["title"])
+    
+    # if len(amazon_jobs) > 0:
+    #     send_email(amazon_jobs)
+    send_email(
+        [
+            {
+                "id": "123456",
+                "company": "Amazon",
+                "posting_date": "April 21, 2026",
+                "title": "Software Engineer Intern",
+                "link": "https://account.amazon.jobs/jobs/10552937/apply",
+                "city": "Seattle",
+                "country": "USA"
+            },
+            {
+                "id": "123456",
+                "company": "RBC",
+                "posting_date": "April 29, 2026",
+                "title": "Software Developer Intern",
+                "link": "https://account.amazon.jobs/jobs/10552937/apply",
+                "city": "Toronto",
+                "country": "CAN"
+            }
+        ]
+    )
